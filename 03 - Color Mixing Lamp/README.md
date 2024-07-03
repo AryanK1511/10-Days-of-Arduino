431474d0b3bb8d66b30bfc96e713176eeb11162a
AryanK1511 <aryankhurana1511@gmail.com>
2024-07-03 09:14:30 -0400

    Add Color mixing lamp project

---
+++ 03 - Color Mixing Lamp/README.md
@@ -0,0 +1,9 @@
+# Color mixing lamp
+
+A project that uses a phototransistor to change the color of an RGB LED according to the colors around the transistor.
+
+<div>
+  <a href="https://youtu.be/9LF6nwGDSTY">
+    <img src="https://img.youtube.com/vi/9LF6nwGDSTY/0.jpg" alt="Watch the video" style="width:560px;">
+  </a>
+</div>
---
+++ 03 - Color Mixing Lamp/code.ino
@@ -0,0 +1,55 @@
+const int greenLEDPin = 9;
+const int redLEDPin = 11;
+const int blueLEDPin = 10;
+
+const int redSensorPin = A0;
+const int greenSensorPin = A1;
+const int blueSensorPin = A2;
+
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
+
+int redSensorValue = 0;
+int greenSensorValue = 0;
+int blueSensorValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(greenLEDPin, OUTPUT);
+  pinMode(redLEDPin, OUTPUT);
+  pinMode(blueLEDPin, OUTPUT);
+}
+
+void loop() {
+  redSensorValue = analogRead(redSensorPin);
+  delay(5);
+  greenSensorValue = analogRead(greenSensorPin);
+  delay(5);
+  blueSensorValue = analogRead(blueSensorPin);
+
+  Serial.print("Raw sensor values \t Red: ");
+  Serial.print(redSensorValue);
+  Serial.print("\t Green: ");
+  Serial.print(greenSensorValue);
+  Serial.print("\t Blue: ");
+  Serial.print(blueSensorValue);
+  Serial.print("\n");
+
+  redValue = redSensorValue / 4;
+  greenValue = greenSensorValue / 4;
+  blueValue = blueSensorValue / 4;
+
+  Serial.print("Mapped sensor values \t Red: ");
+  Serial.print(redValue);
+  Serial.print("\t Green: ");
+  Serial.print(greenValue);
+  Serial.print("\t Blue: ");
+  Serial.print(blueValue);
+  Serial.print("\n");
+
+  analogWrite(redLEDPin, redValue);
+  analogWrite(greenLEDPin, greenValue);
+  analogWrite(blueLEDPin, blueValue);
+  
+}
