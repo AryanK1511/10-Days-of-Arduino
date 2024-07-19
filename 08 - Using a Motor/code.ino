7a9a3dc02f6372d1b4555010a2f3d37b34ebdef9
AryanK1511 <aryankhurana1511@gmail.com>
2024-07-19 03:56:54 -0400

    Update code

--- 08 - Using a Motor/code.ino
+++ 08 - Using a Motor/code.ino
@@ -1,20 +1,23 @@
 const int switchPin = 2;
+const int ledPin = 7;
 const int motorPin = 8;
 int switchState  = 0;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
   pinMode(motorPin, OUTPUT);
   pinMode(switchPin, INPUT);
 }
 
 void loop() {
   switchState = digitalRead(switchPin);
-  Serial.println(switchState);
 
   if (switchState == HIGH) {
     digitalWrite(motorPin, HIGH);
+    digitalWrite(ledPin, HIGH);
   } else {
     digitalWrite(motorPin, LOW);
+    digitalWrite(ledPin, LOW);
   }
 }
